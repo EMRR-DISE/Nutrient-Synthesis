af73984f73f26973b7537a7bef98fc5406262f8b
rosehartman <rosehartman@gmail.com>
2025-06-13 10:31:05 -0700

    organizing tidal parr data

---
+++ DataforKevin.R
@@ -0,0 +1,65 @@
+#organized nutrient dataset for KEvin and the Delta Modeling group
+#Rosemary Hartman
+#2024-10-21
+
+library(tidyverse)
+library(discretewq)
+library(deltamapr)
+library(sf)
+
+
+WQ = wq(Sources = c("EMP", "NCRO", "20mm", "FMWT", "STN", "USGS_CAWSC", "USGS_SFBS"),
+        Start_year = 2000, End_year = 2023)
+
+Micro = filter(WQ, !is.na(Microcystis))
+Nuts = filter(WQ, !is.na(DissNitrateNitrite))
+
+Both = filter(WQ, !is.na(Microcystis) & !is.na(DissNitrateNitrite))
+
+
+subregions = R_EDSM_Subregions_Mahardja
+
+WQsf = filter(WQ, !is.na(Longitude)) %>%
+  st_as_sf(coords = c("Longitude","Latitude"), crs = 4326) %>%
+    st_transform(crs = st_crs(subregions)) %>%
+  st_join(subregions) %>%
+  mutate(SubRegion = case_when(is.na(SubRegion) ~ "Really Far South",
+                        TRUE ~ SubRegion))
+
+
+ggplot()+
+  geom_sf(data = WW_Delta)+
+  geom_sf(data = WQsf, aes(color = SubRegion))
+
+
+#monthly means
+
+WQmonthly = WQsf %>%
+  st_drop_geometry() %>%
+  mutate(Month = month(Date), Year = year(Date)) %>%
+  group_by(SubRegion, Month, Year) %>%
+  summarize(Microcystis = mean(Microcystis, na.rm = T), 
+            Secchi = mean(Secchi, na.rm = T),
+            DissNitrateNitrite = mean(DissNitrateNitrite, na.rm =T),
+            DissAmmonia = mean(DissAmmonia, na.rm = T),
+            pH = mean(pH, na.rm =T),
+            Chl = mean(Chlorophyll, na.rm =T),
+            Conductivity = mean(Conductivity, na.rm =T),
+            Temperature = mean(Temperature, na.rm =T),
+            DissolvedOxygen = mean(DissolvedOxygen, na.rm =T),
+             TurbidityNTU = mean(TurbidityNTU, na.rm =T),
+            TKN = mean(TKN, na.rm =T),
+            Salinity = mean(Salinity, na.rm =T),
+            DissOrthophos = mean(DissOrthophos, na.rm =T),
+            TotPhos = mean(TotPhos, na.rm =T)) %>%
+  mutate(across(c(Microcystis:TotPhos), ~ifelse(is.nan(.), NA, .)))
+
+WQmonthlysub = filter(WQmonthly, !is.na(Microcystis), !is.na(DissNitrateNitrite))
+
+
+#what does this look like?
+
+ggplot(WQmonthlysub, aes(x = DissNitrateNitrite, y = Microcystis))+
+  geom_point()
+
+write.csv(WQmonthlysub, "data/discretewq_monthlymean.csv")
---
+++ code/tidal parr.R
@@ -0,0 +1,48 @@
+#figure out what is going on with the tidal parr data
+
+library(tidyverse)
+library(readxl)
+
+stations = read_csv("data/Bryte Internal Station List.csv") %>%
+  rename(Station = station.wq.bryte.internal)
+
+TidalParr1 = read_excel("data/Tidal Parr 2020_WQ_Bryte.xlsx")%>%
+  mutate(Station = str_trim(str_remove(`Station Number`, "\\(.*\\)")),
+         CollectionDate = mdy_hm(`Collection Date`))
+TidalParr2 = read_csv("data/WQ Data Tidal Parr 2018_2019.csv") %>%
+  mutate(`Rpt Limit` = as.character(`Rpt Limit`), Station = `Station Name`,
+CollectionDate = mdy_hm(`Collection Date`))
+
+WDLtidalparr = read_excel("data/ALLTidalParr_download02192025.xlsx") %>%
+  mutate(CollectionDate = mdy_hm(`Collection Date`))
+
+Test = bind_rows(TidalParr1, TidalParr2) %>%
+  select(`Station`,`Sample Code`, `CollectionDate`) %>%
+  distinct() 
+
+Test2 = bind_rows(TidalParr1, TidalParr2) %>%
+  select(`Station`, `CollectionDate`) %>%
+  distinct() %>%
+  group_by(CollectionDate) %>%
+  summarize(N = n()) %>%
+  filter(N>1)
+
+test3 = filter(bind_rows(TidalParr1, TidalParr2), CollectionDate %in% Test2$CollectionDate)
+
+TidalParrstations = bind_rows(TidalParr1, TidalParr2) %>%
+    select(`Station`, `Sample Code`, `CollectionDate`) %>%
+  distinct()  %>%
+  full_join(WDLtidalparr, by = c("CollectionDate", "Sample Code"))
+
+foo3 = unique(WDLtidalparr$`CollectionDate`)
+brett = c(unique(TidalParr1$`CollectionDate`), unique(TidalParr2$`CollectionDate`))
+
+notthere = filter(WDLtidalparr, !`CollectionDate` %in% brett)
+there = filter(TidalParrstations, !`CollectionDate` %in% unique(WDLtidalparr$`CollectionDate`))
+#ok, just misisng the equipment blanks and maybe two samples
+
+TidalParrFinal = TidalParrstations %>%
+  select(-`Station Number`, -`Long Station Name`, -`Short Station Name`) %>%
+  rename(`Station Number` = Station)
+
+write.csv(TidalParrFinal, "data/TidalParrFinal.csv")
